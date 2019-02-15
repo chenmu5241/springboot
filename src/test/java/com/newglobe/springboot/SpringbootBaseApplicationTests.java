2e009b7a29971d9e29c573210daa30163d967e00
chenmu5241 <chenmu5241@163.com>
2019-02-15 18:43:16 +0800

    Hello

--- src/test/java/com/newglobe/springboot/SpringbootBaseApplicationTests.java
+++ src/test/java/com/newglobe/springboot/SpringbootBaseApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.alibaba.fastjson.JSON;
 import com.newglobe.service.SysRoleService;
 
 @RunWith(SpringRunner.class)
@@ -16,7 +17,7 @@ public class SpringbootBaseApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		System.out.println(sysRoleService.selectList());
+		System.out.println(JSON.toJSONString(sysRoleService.selectList()));
 	}
 
 }
