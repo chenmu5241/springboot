525f7ccc1155b8bca02a04bd9feac6af2895269e
chenmu5241 <chenmu5241@163.com>
2019-03-28 10:05:46 +0800

    增加文件

--- src/main/java/net/newglobe/app/dao/read/SysOauthAccountDao.java
+++
@@ -1,8 +0,0 @@
-package net.newglobe.dao.read;
-
-import net.newglobe.model.SysOauthAccount;
-import net.newglobe.mybatis.BaseDao;
-import net.newglobe.mybatis.MyBatisRepository;
-@MyBatisRepository
-public interface SysOauthAccountDao extends BaseDao<SysOauthAccount>{
-}
